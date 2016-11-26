1d845ebbf9d46d28268e1e70155dc979ea1acc46
Kiran James <kiranjamespulikkottil@gmail.com>
2016-11-26 21:34:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/KiranProfile/user-management.js', function (req, res) {
 });
 
 app.get('/KiranProfile/jquery-3.1.1.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'KiranProfile', 'jquery-1.11.1.js'));
+  res.sendFile(path.join(__dirname, 'KiranProfile', 'jquery-3.1.1.js'));
 });
 
 
