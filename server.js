a60075c09f2fa084e6dfa6ecd3edddb62fd11bb7
Kiran James <kiranjamespulikkottil@gmail.com>
2016-11-26 16:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'KiranProfile', 'WebApp.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'KiranProfile', 'custom.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'KiranProfile', 'kiran.jpg'));
 });
 
 
