c14480cca0c60c4cfe64577d3a8191d316e3316f
Kiran James <kiranjamespulikkottil@gmail.com>
2016-11-26 16:14:19 +0530

    MyWebApp

---
+++ KiranProfile/user-management.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+
+        $('#skills-content').hide();
+        $('#education-content').hide();
+        $('#achivements-content').hide();
+        $('#profile-content').show();
+        $('.content li').removeClass('clikked-tab');
+        $('#profile').addClass('clikked-tab');
+
+
+        $('#profile').click(function () {
+            $('#skills-content').hide();
+            $('#education-content').hide();
+            $('#achivements-content').hide();
+            $('#profile-content').show();
+            $('.content li').removeClass('clikked-tab');
+            $('#profile').addClass('clikked-tab');
+        });
+        $('#education').click(function () {
+            $('#skills-content').hide();
+            $('#achivements-content').hide();
+            $('#profile-content').hide();
+            $('#education-content').show();
+            $('.content li').removeClass('clikked-tab');
+            $('#education').addClass('clikked-tab');
+        });
+        $('#skills').click(function () {
+            $('#education-content').hide();
+            $('#achivements-content').hide();
+            $('#profile-content').hide();
+            $('#skills-content').show();
+            $('.content li').removeClass('clikked-tab');
+            $('#skills').addClass('clikked-tab');
+        });
+        $('#achivements').click(function () {
+            $('#skills-content').hide();
+            $('#education-content').hide();
+            $('#profile-content').hide();
+            $('#achivements-content').show();
+            $('.content li').removeClass('clikked-tab');
+            $('#achivements').addClass('clikked-tab');
+        });
+});
